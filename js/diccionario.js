a51cadc0eb05550cf7c3390c30b1170ae4f8bfc2
JNRivadeneira <jnrivadeneira@gmail.com>
2023-04-01 22:36:47 -0300

    chore: Coloca diccionario en archivo aparte.

---
+++ js/diccionario.js
@@ -0,0 +1,261 @@
+let palabrasData = {
+  a: [
+    {
+      pista: "Comienza con A",
+      palabra: ["abrevadero"],
+      definicion: "Estanque, pilón o paraje del río, arroyo o manantial a propósito para dar de beber al ganado."
+    },
+    {
+      pista: "Comienza con A",
+      palabra: ["astucia"],
+      definicion: "Cualidad de astuto."
+    },
+    {
+      pista: "Comienza con A",
+      palabra: ["alambre"],
+      definicion: "Hilo de cualquier metal, obtenido por trefilado."
+    },
+    {
+      pista: "Comienza con A",
+      palabra: ["anabólico", "anabólica"],
+      definicion: "Perteneciente o relativo al anabolismo."
+    },
+  ],
+  b: [
+    {
+      pista: "Comienza con B",
+      palabra: ["banalidad"],
+      definicion: "Cualidad de banal."
+    },
+    {
+      pista: "Comienza con B",
+      palabra: ["binario"],
+      definicion: "Compuesto de dos elementos, unidades o guarismos."
+    },
+    {
+      pista: "Comienza con B",
+      palabra: ["boicot"],
+      definicion: "Acción que se dirige contra una persona o entidad para obstaculizar el desarrollo o funcionamiento de una determinada actividad social o comercial."
+    },
+    {
+      pista: "Comienza con B",
+      palabra: ["butaca"],
+      definicion: "Asiento con brazos y respaldo, semejante a un sillón pero generalmente menos voluminoso."
+    },
+  ],
+  c: [
+    {
+      pista: "Comienza con C",
+      palabra: ["carcajada"],
+      definicion: "Risa impetuosa y ruidosa."
+    },
+    {
+      pista: "Comienza con C",
+      palabra: ["criollo", "criolla"],
+      definicion: "Autóctono o propio de un país hispanoamericano, o del conjunto de ellos."
+    },
+    {
+      pista: "Comienza con C",
+      palabra: ["carbonilla"],
+      definicion: "Carbón mineral menudo que, como residuo, suele quedar al mover y trasladar el grueso."
+    },
+    {
+      pista: "Comienza con C",
+      palabra: ["cuaco"],
+      definicion: "Harina de la raíz de la yuca."
+    },
+    
+  ],
+  d: [
+    {
+      pista: "Comienza con D",
+      palabra: ["duna"],
+      definicion: "Colina de arena movediza que en los desiertos y en las playas forma y empuja el viento."
+    },
+    {
+      pista: "Comienza con D",
+      palabra: ["dromedario"],
+      definicion: "Artiodáctilo rumiante, propio de Arabia y del norte de África, muy semejante al camello, del cual se distingue principalmente por no tener más que una giba adiposa en el dorso."
+    },
+    {
+      pista: "Comienza con D",
+      palabra: ["dinosaurio"],
+      definicion: "Reptil fósil, propio del Mesozoico, generalmente de gran tamaño, cabeza pequeña, cuello largo, cola robusta y larga, y extremidades posteriores más largas que las anteriores."
+    },
+    {
+      pista: "Comienza con D",
+      palabra: ["docenario", "docenaria"],
+      definicion: "Que consta de doce unidades o elementos constitutivos."
+    },
+    
+  ],
+  e: [
+    {
+      pista: "Comienza con E",
+      palabra: ["encabar"],
+      definicion: "Poner cabo o mango a una herramienta."
+    },
+    {
+      pista: "Comienza con E",
+      palabra: ["enjuague"],
+      definicion: "Acción de enjuagar."
+    },
+    {
+      pista: "Comienza con E",
+      palabra: ["estilete"],
+      definicion: "Puñal de hoja muy estrecha y aguda."
+    },
+    {
+      pista: "Comienza con E",
+      palabra: ["estación"],
+      definicion: "Cada una de las cuatro partes o tiempos en que se divide el año."
+    },
+  ],
+  // f: [
+  //   {
+  //     pista: "Comienza con ",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // g: [
+  //   {
+  //     pista: "Comienza con ",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // h: [
+  //   {
+  //     pista: "Comienza con ",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // i: [
+  //   {
+  //     pista: "Comienza con ",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // j: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // k: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // l: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // m: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // n: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // ñ: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // o: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // p: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // q: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // r: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // s: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // t: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // u: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // v: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // x: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // y: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+  // z: [
+  //   {
+  //     pista: "",
+  //     palabra: "",
+  //     definicion: ""
+  //   }
+  // ],
+};
