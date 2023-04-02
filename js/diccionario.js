f5578d3235a4821a5407c8a955b4385092563e7e
JNRivadeneira <jnrivadeneira@gmail.com>
2023-04-02 02:08:22 -0300

    enh: Agrega nuevas palabras al dic.

--- js/diccionario.js
+++ js/diccionario.js
@@ -111,111 +111,336 @@ let palabrasData = {
       definicion: "Cada una de las cuatro partes o tiempos en que se divide el año."
     },
   ],
-  // f: [
-  //   {
-  //     pista: "Comienza con ",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // g: [
-  //   {
-  //     pista: "Comienza con ",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // h: [
-  //   {
-  //     pista: "Comienza con ",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // i: [
-  //   {
-  //     pista: "Comienza con ",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // j: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // k: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // l: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // m: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // n: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // ñ: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // o: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // p: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // q: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // r: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
-  // s: [
-  //   {
-  //     pista: "",
-  //     palabra: "",
-  //     definicion: ""
-  //   }
-  // ],
+  f: [
+    {
+      pista: "Comienza con F",
+      palabra: ["ficticio"],
+      definicion: "Fingido, imaginario o falso."
+    },
+    {
+      pista: "Comienza con F",
+      palabra: ["formidable"],
+      definicion: "Muy temible y que infunde asombro y miedo."
+    },
+    {
+      pista: "Comienza con F",
+      palabra: ["falacia"],
+      definicion: "Engaño, fraude o mentira."
+    },
+    {
+      pista: "Comienza con F",
+      palabra: ["frasco"],
+      definicion: "Vaso de cuello recogido, hecho de vidrio u otra materia, que sirve para contener líquidos, sustancias en polvo, comprimidos, etc."
+    },
+  ],
+  g: [
+    {
+      pista: "Comienza con G",
+      palabra: ["gótico", "gótica"],
+      definicion: "Perteneciente o relativo a los godos."
+    },
+    {
+      pista: "Comienza con G",
+      palabra: ["gol"],
+      definicion: "En el fútbol y otros deportes, entrada del balón en la portería."
+    },
+    {
+      pista: "Comienza con G",
+      palabra: ["gula"],
+      definicion: "Exceso en la comida o bebida, y apetito desordenado de comer y beber."
+    },
+    {
+      pista: "Comienza con G",
+      palabra: ["gulag"],
+      definicion: "En la antigua Unión Soviética, campo de concentración."
+    },
+  ],
+  h: [
+    {
+      pista: "Comienza con H",
+      palabra: ["hombro"],
+      definicion: "Parte superior y lateral del tronco del ser humano y de los cuadrumanos, de donde nace el brazo."
+    },
+    {
+      pista: "Contiene H",
+      palabra: ["adherencia"],
+      definicion: "Unión física, pegadura de las cosas."
+    },
+    {
+      pista: "Comienza con H",
+      palabra: ["horquilla"],
+      definicion: "Pieza que en las bicicletas, motocicletas y vehículos de similares características va desde el eje de la rueda delantera hasta el manillar."
+    },
+    {
+      pista: "Comienza con H",
+      palabra: ["hummus", "humus"],
+      definicion: "Pasta de garbanzos, típica de la cocina árabe, aderezada generalmente con aceite de oliva, zumo de limón, crema de sésamo y ajo."
+    },
+  ],
+  i: [
+    {
+      pista: "Comienza con I",
+      palabra: ["indicio"],
+      definicion: "Fenómeno que permite conocer o inferir la existencia de otro no percibido."
+    },
+    {
+      pista: "Comienza con I",
+      palabra: ["inframundo"],
+      definicion: "Mundo de los muertos y de los espíritus."
+    },
+    {
+      pista: "Comienza con I",
+      palabra: ["imberbe"],
+      definicion: "Que todavía no tiene barba."
+    },
+    {
+      pista: "Comienza con I",
+      palabra: ["irracional"],
+      definicion: "Opuesto a la razón o ajeno a ella."
+    },
+  ],
+  j: [
+    {
+      pista: "Comienza con J",
+      palabra: ["jerarca", "jefe", "jefa"],
+      definicion: "Persona que tiene elevada categoría en una organización, una empresa, etc."
+    },
+    {
+      pista: "Comienza con J",
+      palabra: ["justa"],
+      definicion: "Pelea o combate singular, a caballo y con lanza."
+    },
+    {
+      pista: "Comienza con J",
+      palabra: ["jardín"],
+      definicion: "Terreno donde se cultivan plantas con fines ornamentales."
+    },
+    {
+      pista: "Comienza con J",
+      palabra: ["jeta"],
+      definicion: "Boca saliente por su configuración o por tener los labios muy abultados."
+    },
+  ],
+  k: [
+    {
+      pista: "Contiene K",
+      palabra: ["tokiota"],
+      definicion: "Natural de Tokio, capital del Japón."
+    },
+    {
+      pista: "Contiene K",
+      palabra: ["pakistaní", "paquistaní"],
+      definicion: "Natural del Pakistán, país de Asia."
+    },
+    {
+      pista: "Contiene K",
+      palabra: ["póker", "póquer"],
+      definicion: "Juego de naipes con baraja francesa, en el que se reparten cinco cartas a cada jugador, se hacen apuestas y descartes, y gana quien reúne la combinación superior entre las varias establecidas."
+    },
+    {
+      pista: "Comienza con K",
+      palabra: ["kimono", "quimono"],
+      definicion: "Túnica de origen japonés, de mangas anchas y largas, abierta por delante y que se ciñe, cruzándola, mediante un cinturón."
+    },
+  ],
+  l: [
+    {
+      pista: "Comienza con L",
+      palabra: ["limbo"],
+      definicion: "En la doctrina tradicional católica, lugar adonde irían las almas de quienes mueren sin el bautismo antes de tener uso de razón."
+    },
+    {
+      pista: "Comienza con L",
+      palabra: ["lumbalgia"],
+      definicion: "Dolor en la región lumbar."
+    },
+    {
+      pista: "Comienza con L",
+      palabra: ["lejanía"],
+      definicion: "Parte remota o distante de un lugar, de un paisaje o de una vista panorámica."
+    },
+    {
+      pista: "Comienza con L",
+      palabra: ["lejía"],
+      definicion: "Solución de sales alcalinas en agua, que se utiliza en limpieza como desinfectante y blanqueador doméstico."
+    },
+  ],
+  m: [
+    {
+      pista: "Comienza con M",
+      palabra: ["mañero"],
+      definicion: "Sagaz, astuto."
+    },
+    {
+      pista: "Comienza con M",
+      palabra: ["mosto"],
+      definicion: "Zumo exprimido de la uva, antes de fermentar y hacerse vino."
+    },
+    {
+      pista: "Comienza con M",
+      palabra: ["mercado"],
+      definicion: "Sitio público destinado permanentemente, o en días señalados, para vender, comprar o permutar bienes o servicios."
+    },
+    {
+      pista: "Comienza con M",
+      palabra: ["multa"],
+      definicion: "Sanción administrativa o penal que consiste en la obligación de pagar una cantidad determinada de dinero."
+    },
+  ],
+  n: [
+    {
+      pista: "Comienza con N",
+      palabra: ["natural"],
+      definicion: "Nativo de un lugar."
+    },
+    {
+      pista: "Comienza con N",
+      palabra: ["nocivo"],
+      definicion: "Dañoso, pernicioso, perjudicial."
+    },
+    {
+      pista: "Comienza con N",
+      palabra: ["negro"],
+      definicion: "Dicho de un cuerpo: Que no refleja ninguna radiación visible."
+    },
+    {
+      pista: "Comienza con N",
+      palabra: ["nudo"],
+      definicion: "Lazo que se estrecha y cierra de modo que con dificultad se pueda soltar por sí solo, y que cuanto más se tira de cualquiera de los dos cabos, más se aprieta."
+    },
+  ],
+  ñ: [
+    {
+      pista: "Comienza con Ñ",
+      palabra: ["ñoqui"],
+      definicion: "Masa hecha con patatas mezcladas con harina de trigo, mantequilla, leche, huevo y queso rallado, dividida en trocitos, que se cuecen en agua hirviendo con sal."
+    },
+    {
+      pista: "Comienza con Ñ",
+      palabra: ["ñoño"],
+      definicion: "Dicho de una cosa: Sosa, de poca sustancia."
+    },
+    {
+      pista: "Contiene Ñ",
+      palabra: ["antaño"],
+      definicion: "En un tiempo pasado."
+    },
+    {
+      pista: "Contiene Ñ",
+      palabra: ["rebaño"],
+      definicion: "Conjunto de personas que se mueven gregariamente o se dejan dirigir en sus opiniones, gustos, etc."
+    },
+  ],
+  o: [
+    {
+      pista: "Comienza con O",
+      palabra: ["ordinario"],
+      definicion: "Común, regular y que sucede habitualmente."
+    },
+    {
+      pista: "Comienza con O",
+      palabra: ["ola"],
+      definicion: "Onda de gran amplitud que se forma en la superficie de las aguas."
+    },
+    {
+      pista: "Comienza con O",
+      palabra: ["ojiva"],
+      definicion: "Figura formada por dos arcos de círculo iguales, que se cortan en uno de sus extremos y volviendo la concavidad el uno al otro."
+    },
+    {
+      pista: "Comienza con O",
+      palabra: ["osteolito"],
+      definicion: "Hueso fósil."
+    },
+  ],
+  p: [
+    {
+      pista: "Comienza con P",
+      palabra: ["pirotecnia"],
+      definicion: "Técnica de la fabricación y utilización de materiales explosivos o fuegos artificiales."
+    },
+    {
+      pista: "Comienza con P",
+      palabra: ["pirata"],
+      definicion: "Persona que, junto con otras de igual condición, se dedica al abordaje de barcos en el mar para robar."
+    },
+    {
+      pista: "Comienza con P",
+      palabra: ["pamela"],
+      definicion: "Sombrero de paja, bajo de copa y ancho de alas, que usan las mujeres, especialmente en el verano."
+    },
+    {
+      pista: "Comienza con P",
+      palabra: ["pórtico"],
+      definicion: "Espacio cubierto y con columnas situado delante de los templos u otros edificios."
+    },
+  ],
+  q: [
+    {
+      pista: "Comienza con Q",
+      palabra: ["quilate"],
+      definicion: "Unidad de peso para las perlas y piedras preciosas, que equivale a 200 mg."
+    },
+    {
+      pista: "Comienza con Q",
+      palabra: ["quilla"],
+      definicion: " Pieza de madera o hierro, que va de popa a proa por la parte inferior del barco y en que se asienta toda su armazón."
+    },
+    {
+      pista: "Contiene Q",
+      palabra: ["estanque"],
+      definicion: "Balsa construida para recoger el agua, con fines utilitarios, como proveer al riego, criar peces, etc., o meramente ornamentales."
+    },
+    {
+      pista: "Contiene Q",
+      palabra: ["aquelarre"],
+      definicion: "Junta o reunión nocturna de brujos y brujas, con la supuesta intervención del demonio ordinariamente en figura de macho cabrío, para sus prácticas mágicas o supersticiosas."
+    },
+  ],
+  r: [
+    {
+      pista: "Comienza con R",
+      palabra: ["risco"],
+      definicion: "Peñasco alto y escarpado, difícil y peligroso para andar por él."
+    },
+    {
+      pista: "Comienza con R",
+      palabra: ["ruta"],
+      definicion: "Camino o dirección que se toma para un propósito."
+    },
+    {
+      pista: "Comienza con R",
+      palabra: ["romo"],
+      definicion: "Que carece de punta o filo."
+    },
+    {
+      pista: "Comienza con R",
+      palabra: ["racha"],
+      definicion: "En cualquier actividad, período breve de fortuna o desgracia."
+    },
+  ],
+  s: [
+    {
+      pista: "Comienza con S",
+      palabra: ["serenata"],
+      definicion: "Música en la calle o al aire libre y durante la noche, para festejar a alguien."
+    },
+    {
+      pista: "Comienza con S",
+      palabra: ["sien"],
+      definicion: "Cada una de las dos partes laterales de la cabeza situadas entre la frente, la oreja y la mejilla."
+    },
+    {
+      pista: "Comienza con S",
+      palabra: ["solsticio"],
+      definicion: "Cada uno de los dos momentos anuales en que el Sol se halla en uno de los dos trópicos, lo cual sucede del 21 al 22 de junio para el de Cáncer, y del 21 al 22 de diciembre para el de Capricornio, y en los que la diferencia entre la duración del día y de la noche es mayor."
+    },
+    {
+      pista: "Comienza con S",
+      palabra: ["sirena"],
+      definicion: "Ninfa marina con busto de mujer y cuerpo de ave según la tradición grecolatina, y con cuerpo de pez en otras tradiciones, que extraviaba a los navegantes atrayéndolos con la dulzura de su canto."
+    },
+  ],
   // t: [
   //   {
   //     pista: "",
