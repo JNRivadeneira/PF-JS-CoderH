7cd671c13fa056fc4f1fac82f08b13f0026ca971
JNRivadeneira <jnrivadeneira@gmail.com>
2023-04-15 00:02:31 -0300

    agrega funcion saltapalabra

--- js/main.js
+++ js/main.js
@@ -19,8 +19,12 @@ class Diccionario{
 class Rosco{
   constructor(setPalabras){
     this.setPalabras = setPalabras;
+    this.punteroLetraActual = 0;
+    this.punteroLetraSiguiente = 1;
+    this.letrasRestantes = Object.keys(setPalabras);
   }
   
+
   render(){
     let divRosco = document.getElementById("rosco");
     // let rosco = "";
@@ -33,7 +37,7 @@ class Rosco{
       <button id="circulo-f" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="top: 161.34px; right: 16.24px">F</button>
       <button id="circulo-g" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="top: 219.86px; right: 1.82px">G</button>
       <button id="circulo-h" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="bottom: 219.86px; right: 1.82px">H</button>
-      <button id="circulo-i" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="bottom: 161.34px; right: 16.24px">I</button>
+      <button id="circulo-i" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-[20px] py-2 text-center" style="bottom: 161.34px; right: 16.24px">I</button>
       <button id="circulo-j" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="bottom: 107.98px; right: 44.25px">J</button>
       <button id="circulo-k" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="bottom: 62.87px; right: 84.21px">K</button>
       <button id="circulo-l" disabled class="absolute text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 shadow-lg shadow-[#2056df] font-semibold rounded-full text-lg px-4 py-2 text-center" style="bottom: 28.63px; right: 133.81px">L</button>
@@ -62,19 +66,18 @@ class Rosco{
     // console.log(divRosco)
   }
 
-  setStatusRespuestas(Correctas, Incorrectas, sinContestar){
+  // setStatusRespuestas(Correctas, Incorrectas, sinContestar){
   
-  };
+  // };
 
-  getStatusRespuestas(){
-    let status = {
-      correctas: 0,
-      incorrectas: 0
-    };
-  }
+  // getStatusRespuestas(){
+  //   let status = {
+  //     correctas: 0,
+  //     incorrectas: 0
+  //   };
+  // }
 
   setVerde(letra){
-    // document.getElementById(`circulo-${letra}`).className = "text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 dark:ring-blue-800 shadow-lg shadow-neutral-500/50 dark:shadow-lg dark:shadow-neutral-800/80 font-semibold rounded-full text-lg px-4 py-2 text-center mr-2 mb-2 ";
     document.getElementById(`circulo-${letra}`).classList.add("from-green-500", "via-green-600", "to-green-700", "focus:ring-green-300", "shadow-[#158f43]");
     document.getElementById(`circulo-${letra}`).classList.remove("from-blue-500", "via-blue-600", "to-blue-700", "focus:ring-blue-300", "shadow-[#2056df]");
   };
@@ -85,11 +88,20 @@ class Rosco{
     document.getElementById(`circulo-${letra}`).classList.remove("from-blue-500", "via-blue-600", "to-blue-700", "focus:ring-blue-300", "shadow-[#2056df]");
   };
 
+  saltaPalabra(){
+    (this.punteroLetraActual == this.letrasRestantes.length - 1)? this.punteroLetraActual = 0 : this.punteroLetraActual++;
+    (this.punteroLetraSiguiente == this.letrasRestantes.length - 1)? this.punteroLetraSiguiente = 0 : this.punteroLetraSiguiente++;
+
+    return this.letrasRestantes.at(this.punteroLetraActual);
+
+  }
 };
 
 // creacion de la partida
 const diccionario1 = new Diccionario(palabrasData);
 let set1 = diccionario1.getSet();
+let keys = Object.keys(set1);
+
 
 const rosco1 = new Rosco(set1);
 rosco1.render();
@@ -123,15 +135,75 @@ function start(){
   let parrafoPista = document.getElementById("parrafo-pista");
   let parrafoDefinicion = document.getElementById("parrafo-definicion");
   let inputRespuesta = document.getElementById("input-respuesta");
-  for (const letra in set1) {
-    parrafoPista.innerText = set1[letra].pista;
-    parrafoDefinicion.innerText = set1[letra].definicion;
-    if (set1[letra].palabra.includes(inputRespuesta.value.toLowerCase())){
-      alert("🥳 CORRECTO!");
-      marcador1.correctas++;
-      rosco1.setVerde(letra);
-    }
+  
+  show("rosco");
+
+  while (keys.length > 0){
+  
+  }
+  console.log(keys.at(2));
+
+
+  // for (const letra in set1) {
+  //   parrafoPista.innerText = set1[letra].pista;
+  //   parrafoDefinicion.innerText = set1[letra].definicion;
+  //   inputRespuesta.addEventListener("keydown", (event) => {
+  //     console.log("prueba");
+  //   });
+  //   if (set1[letra].palabra.includes(inputRespuesta.value.toLowerCase())){
+  //     // alert("🥳 CORRECTO!");
+  //     marcador1.correctas++;
+  //     rosco1.setVerde(letra);
+  //   }
+  //   else {
+  //     // alert(`❌ INCORRECTO!\n\nLa respuesta correcta es:       ${set1[letra].palabra.join(" / ")}`);
+  //     marcador1.incorrectas++; 
+  //     rosco1.setRojo(letra);
+  //   }
+  // }
+}
+
+function show(element){
+  switch (element) {
+    case "menu":
+      let menu = document.getElementById("menu-nuevo-juego");
+      menu.classList.remove("hidden");
+      break;
+    case "rosco":
+      let rosco = document.getElementById("div-rosco");
+      rosco.classList.remove("hidden");
+      break;
+    default:
+      console.error("No existe el elemento: ", element);
   }
 }
 
-start();
+function hide(element){
+  switch (element) {
+    case "menu":
+      let menu = document.getElementById("menu-nuevo-juego");
+      menu.classList.add("hidden");
+      break;
+    case "rosco":
+      let rosco = document.getElementById("div-rosco");
+      rosco.classList.add("hidden");
+      break;
+    default:
+      console.error("No existe el elemento: ", element);
+  }
+}
+
+
+// function newGame(){
+//   hide("menu");
+//   start();
+// }
+// start();
+
+
+hide("menu");
+show("rosco");
+
+function prueba(){
+  console.log(rosco1.saltaPalabra());
+}
